407f67d10a8449e14928771e4341621b0313788d
boulen_j <boulen_j@566a80d3-3bec-466a-8343-c7fd184418ef>
2011-03-01 09:51:51 +0000

    mise à jour du cron annuel
    git-svn-id: https://subversion.cru.fr/isounagios/trunk@17 566a80d3-3bec-466a-8343-c7fd184418ef

--- sources/cron/cron_yearly.php
+++ sources/cron/cron_yearly.php
@@ -9,18 +9,26 @@ require BASE.'/php/common_database.php';
 $year = getdate(TIME);
 $year = $year['year'];
 
-$sql = "UPDATE events SET endDate = ".TIME." WHERE endDate IS NULL";
-$db->query($sql);
-
-$sql = "UPDATE events_nagios SET endDate = ".TIME." WHERE endDate IS NULL";
+$sql = "UPDATE events SET endDate = ".(TIME-1)." WHERE endDate IS NULL";
 $db->query($sql);
 
 if(!is_file(BASE.'/database/isou'.$year['year'].'.sqlite3')){
 	if(copy(BASE.'/database/isou.sqlite3', BASE.'/database/isou'.$year['year'].'.sqlite3')){
-		$sql = "DELETE FROM events";
-		$db->query($sql);
-		$sql = "DELETE FROM events_nagios";
-		$db->query($sql);
+		$sql = "DELETE FROM events_isou WHERE idEvent = (SELECT idEvent FROM events WHERE endDate < ".TIME." AND endDate IS NOT NULL)";
+		$query = $db->prepare($sql);
+		$query->execute();
+
+		$sql = "DELETE FROM events_nagios WHERE idEvent = (SELECT idEvent FROM events WHERE endDate < ".TIME." AND endDate IS NOT NULL)";
+		$query = $db->prepare($sql);
+		$query->execute();
+
+		$sql = "DELETE FROM events WHERE endDate < ".TIME." AND endDate IS NOT NULL";
+		$query = $db->prepare($sql);
+		$query->execute();
+
+		$sql = "UPDATE events SET beginDate = ".TIME." WHERE beginDate < ".TIME;
+		$query = $db->prepare($sql);
+		$query->execute();
 	}else{
 		add_log(LOG_FILE, 'ISOU', 'ERROR_DB', 'Les bases n\'ont pas pu être dupliquées.');
 	}
