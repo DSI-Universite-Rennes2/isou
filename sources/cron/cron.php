86ce96a108964e584aa580a4e8d3f99b8253cb89
boulen_j <boulen_j@566a80d3-3bec-466a-8343-c7fd184418ef>
2011-03-22 10:49:29 +0000

    suppression d'une fonction dépréciée
    git-svn-id: https://subversion.cru.fr/isounagios/trunk@50 566a80d3-3bec-466a-8343-c7fd184418ef

--- sources/cron/cron.php
+++ sources/cron/cron.php
@@ -4,16 +4,18 @@ $pwd = dirname(__FILE__).'/..';
 
 require $pwd.'/functions.php';
 require $pwd.'/config.php';
+error_reporting(-1);
 require BASE.'/classes/isou/update.functions.php';
 require BASE.'/classes/isou/parser.function.php';
 
 // creation/modification du fichier de log
 $log = update_nagios_to_db();
 
-if(is_a($log,'Exception')){
+if($log instanceof Exception){
 	add_log(LOG_FILE, 'ISOU', 'error', $log->getMessage());
 }
 
+error_reporting(0);
 require BASE.'/classes/isou/json.php';
 
 ?>
