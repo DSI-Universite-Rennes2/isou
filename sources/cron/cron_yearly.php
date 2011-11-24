d6ccd69769c6b7692e42415dcac1a868eab1cc91
Julien Boulen <julien.boulen@uhb.fr>
2011-11-24 16:29:40 +0100

    correction de bugs et réécriture partielle

--- sources/cron/cron_yearly.php
+++ sources/cron/cron_yearly.php
@@ -1,28 +1,32 @@
 <?php
 
-$year = getdate(TIME);
-$year = $year['year'];
+$year = intval(strftime('%Y', TIME));
 
-$sql = "UPDATE events SET endDate = ".(TIME-1)." WHERE endDate IS NULL";
-$db->query($sql);
+if(!is_file(BASE.'/database/isou-'.($year-1).'.sqlite3')){
+	$newyear = mktime(0, 0, 0, 1, 1, $year);
+	$oldyear = mktime(23, 59, 59, 12, 31, $year-1);
 
-if(!is_file(BASE.'/database/isou'.$year['year'].'.sqlite3')){
-	if(copy(BASE.'/database/isou.sqlite3', BASE.'/database/isou'.$year['year'].'.sqlite3')){
-		$sql = "DELETE FROM events_isou WHERE idEvent = (SELECT idEvent FROM events WHERE endDate < ".TIME." AND endDate IS NOT NULL)";
+	// fermer tous les évènements en cours
+	$sql = "UPDATE events SET endDate = ? WHERE endDate IS NULL";
+	$query = $db->prepare($sql);
+	$query->execute(array($oldyear));
+
+	if(copy(BASE.'/database/isou.sqlite3', BASE.'/database/isou-'.($year-1).'.sqlite3')){
+		$sql = "DELETE FROM events_isou WHERE idEvent = (SELECT idEvent FROM events WHERE endDate < ? AND endDate IS NOT NULL)";
 		$query = $db->prepare($sql);
-		$query->execute();
+		$query->execute(array($newyear));
 
-		$sql = "DELETE FROM events_nagios WHERE idEvent = (SELECT idEvent FROM events WHERE endDate < ".TIME." AND endDate IS NOT NULL)";
+		$sql = "DELETE FROM events_nagios WHERE idEvent = (SELECT idEvent FROM events WHERE endDate < ? AND endDate IS NOT NULL)";
 		$query = $db->prepare($sql);
-		$query->execute();
+		$query->execute(array($newyear));
 
-		$sql = "DELETE FROM events WHERE endDate < ".TIME." AND endDate IS NOT NULL";
+		$sql = "DELETE FROM events WHERE endDate < ? AND endDate IS NOT NULL";
 		$query = $db->prepare($sql);
-		$query->execute();
+		$query->execute(array($newyear));
 
-		$sql = "UPDATE events SET beginDate = ".TIME." WHERE beginDate < ".TIME;
+		$sql = "UPDATE events SET beginDate = ? WHERE beginDate < ?";
 		$query = $db->prepare($sql);
-		$query->execute();
+		$query->execute(array($newyear, $newyear));
 	}else{
 		add_log(LOG_FILE, 'ISOU', 'ERROR_DB', 'Les bases n\'ont pas pu être dupliquées.');
 	}
