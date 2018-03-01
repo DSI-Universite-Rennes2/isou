2b1cd3ca13822dfa30246915b3e0e04c07125991
Julien Boulen <julien.boulen@univ-rennes2.fr>
2018-03-01 11:02:08 +0100

    fix(configuration): améliore la page de diagnostic
    - corrige le chemin du fichier de config.php
    - détecte la présence de l'ancien fichier config.php
    - corrige les messages d'erreur
    - corrige l'habillage css pour les erreurs
    - ajoute une rubrique concernant le cron

--- html/configuration/diagnostics.tpl
+++ html/configuration/diagnostics.tpl
@@ -1,11 +1,13 @@
 {foreach $errors as $type => $details}
 	<h2>{$type}</h2>
 	{if isset($details[0])}
+	<div class="alert alert-danger">
 	<ul>
 		{foreach $details as $detail}
 		<li>{$detail}</li>
 		{/foreach}
 	</ul>
+	</div>
 	{else}
 		<p class="alert alert-info">Aucune erreur détectée.</p>
 	{/if}
--- php/configuration/diagnostics.php
+++ php/configuration/diagnostics.php
@@ -5,28 +5,40 @@ $TITLE = NAME.' - Configuration de l\'authentification';
 $errors = array();
 $errors['Fichier config.php'] = array();
 $errors['Base de données'] = array();
+$errors['Crons'] = array();
 
 // Vérification du fichier config.php.
-$config = file_get_contents(PUBLIC_PATH.'/config.php');
+$config = file_get_contents(PRIVATE_PATH.'/config.php');
 
 if(preg_match('/define\(.NAME/', $config) === 1){
-	$errors['Fichier config.php'][] = 'La constante <code>NAME</code> est n\'est plus utilisée. Vous pouvez la supprimer du fichier config.php';
+	$errors['Fichier config.php'][] = 'La constante <code>NAME</code> est obsolète. Vous pouvez la supprimer du fichier config.php';
 }
 
 if(preg_match('/define\(.HEADER/', $config) === 1){
-	$errors['Fichier config.php'][] = 'La constante <code>HEADER</code> est n\'est plus utilisée. Vous pouvez la supprimer du fichier config.php';
+	$errors['Fichier config.php'][] = 'La constante <code>HEADER</code> est obsolète. Vous pouvez la supprimer du fichier config.php';
 }
 
 if(preg_match('/define\(.STATUSDAT_URL/', $config) === 1){
-	$errors['Fichier config.php'][] = 'La constante <code>STATUSDAT_URL</code> est n\'est plus utilisée. Vous pouvez la supprimer du fichier config.php';
+	$errors['Fichier config.php'][] = 'La constante <code>STATUSDAT_URL</code> est obsolète. Vous pouvez la supprimer du fichier config.php';
 }
 
 if(preg_match('/define\(.DB_STAT_PATH/', $config) === 1){
-	$errors['Fichier config.php'][] = 'La constante <code>DB_STAT_PATH</code> est n\'est plus utilisée. Vous pouvez la supprimer du fichier config.php';
+	$errors['Fichier config.php'][] = 'La constante <code>DB_STAT_PATH</code> est obsolète. Vous pouvez la supprimer du fichier config.php';
+}
+
+if (is_file(PUBLIC_PATH.'/config.php') === true) {
+    $errors['Fichier config.php'][] = 'Le fichier <code>'.PUBLIC_PATH.'/config.php</code> n\'est plus utilisé. Vous pouvez le supprimer.';
 }
 
 // Vérification de la base de données.
 // TODO.
+
+// Vérification du cron.
+if (isset($CFG['last_cron_update']) === false || empty($CFG['last_cron_update']) === true) {
+    $errors['Crons'][] = 'Le fichier cron.php ne semble pas être appelé régulièrement.';
+} else if ((TIME - 10 * 60) > $CFG['last_cron_update']) {
+    $errors['Crons'][] = 'La dernière exécution du cron date du '.strftime('%c', $CFG['last_cron_update']);
+}
 
 $smarty->assign('errors', $errors);
 
