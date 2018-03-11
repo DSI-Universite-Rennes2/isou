3e7b332e05600f2bf9bb459aebbf3e6436a2b3d4
Julien Boulen <julien.boulen@univ-rennes2.fr>
2018-03-11 17:50:55 +0100

    fix(database): corrige l'habillage de la page d'erreur de connexion à la base de données

---
+++ html/common/error_database.tpl
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>ISOU - Problème d'accès à la base de données</title>
+
+    <link rel="shortcut icon" type="image/png" href="{$smarty.const.URL}/themes/bootstrap/favicon.png" />
+
+    <link href="{$smarty.const.URL}/themes/bootstrap/css/bootstrap.min.css" type="text/css" media="screen" rel="stylesheet" />
+</head>
+<body role="document">
+
+    <div class="container">
+        <header class="page-header" role="banner">
+            <h1 id="isou-header">ISOU</h1>
+        </header>
+
+        <p class="alert alert-danger">La base de données est inaccessible.</p>
+    </div>
+
+</body>
+</html>
--- php/common/database.php
+++ php/common/database.php
@@ -1,40 +1,21 @@
 <?php
 
-try{
-	if(!is_file(substr(DB_PATH, 7))){
-		throw new PDOException(DB_PATH.' n\'existe pas.');
-	}
-	$DB = new PDO(DB_PATH, '', '');
-}catch(PDOException $e){
-	$menuId = null;
+try {
+    if (is_file(substr(DB_PATH, 7)) === false) {
+        throw new PDOException(DB_PATH.' n\'existe pas.');
+    }
+    $DB = new PDO(DB_PATH, '', '');
+} catch(PDOException $exception) {
+    if (defined('STDIN') === true) {
+        exit(1);
+    }
 
-	$smarty->assign('URL', URL);
-	$smarty->assign('title', 'Isou');
-	$smarty->assign('script', '');
-	$smarty->assign('css', '');
-	$smarty->assign('annonce', '<p id="annonce">La base de données d\'Isou est momentanément inaccessible. Veuillez réessayer ultérieurement.</p>');
+    $smarty->display('common/error_database.tpl');
 
-	if(count($_GET)>0){
-		$connexion_url = get_base_url('full', HTTPS).'&amp;';
-	}else{
-		$connexion_url = get_base_url('full', HTTPS).'?';
-	}
-	$smarty->assign('connexion_url', $connexion_url);
+    $LOGGER->addError($exception->getMessage());
 
-	$smarty->assign('page', $PAGE_NAME);
-	$smarty->assign('is_admin', $IS_ADMIN);
-	$smarty->assign('menu', $MENU);
+    // Close PDO connection.
+    $DB = null;
 
-	$smarty->display('html_head.tpl');
-	$smarty->display('html_body_header.tpl');
-	$smarty->display('html_body_footer.tpl');
-
-	$LOGGER->addError($e->getMessage());
-
-	// close pdo connection
-	$DB = null;
-
-	exit(0);
+    exit(1);
 }
-
-?>
