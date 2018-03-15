ae029314e7500d59b5ac786501d3080c89cc0477
Julien Boulen <julien.boulen@univ-rennes2.fr>
2018-03-15 14:43:04 +0100

    feat(themes): ajoute un attribut de versionning dans les CSS des thèmes

--- www/themes/bootstrap/theme.php
+++ www/themes/bootstrap/theme.php
@@ -1,20 +1,24 @@
 <?php
 
-$STYLES[] = new \Isou\Helpers\Style(URL.'/themes/bootstrap/css/bootstrap.min.css');
-$STYLES[] = new \Isou\Helpers\Style(URL.'/themes/bootstrap/css/common.css');
+use Isou\Helpers\Style;
+
+$theme_version = '1';
+
+$STYLES[] = new Style(URL.'/themes/bootstrap/css/bootstrap.min.css?v='.$theme_version);
+$STYLES[] = new Style(URL.'/themes/bootstrap/css/common.css?v='.$theme_version);
 
 if (isset($current_page->url) === true) {
     switch ($current_page->url) {
         case 'actualite':
-            $STYLES[] = new \Isou\Helpers\Style(URL.'/themes/bootstrap/css/news.css');
+            $STYLES[] = new Style(URL.'/themes/bootstrap/css/news.css?v='.$theme_version);
             break;
         case 'calendrier':
-            $STYLES[] = new \Isou\Helpers\Style(URL.'/themes/bootstrap/css/calendar.css');
+            $STYLES[] = new Style(URL.'/themes/bootstrap/css/calendar.css?v='.$theme_version);
             break;
         case 'dependances':
-            $STYLES[] = new \Isou\Helpers\Style(URL.'/themes/bootstrap/css/dependencies.css');
+            $STYLES[] = new Style(URL.'/themes/bootstrap/css/dependencies.css?v='.$theme_version);
             break;
         case 'rss':
-            $STYLES[] = new \Isou\Helpers\Style(URL.'/themes/bootstrap/css/rss.css');
+            $STYLES[] = new Style(URL.'/themes/bootstrap/css/rss.css?v='.$theme_version);
     }
 }
--- www/themes/classic/theme.php
+++ www/themes/classic/theme.php
@@ -1,58 +1,62 @@
 <?php
 
-$STYLES[] = new \Isou\Helpers\Style(URL.'/themes/classic/styles/common.css');
-$STYLES[] = new \Isou\Helpers\Style(URL.'/themes/classic/styles/menu.css');
+use Isou\Helpers\Style;
+
+$theme_version = '1';
+
+$STYLES[] = new Style(URL.'/themes/classic/styles/common.css?v='.$theme_version);
+$STYLES[] = new Style(URL.'/themes/classic/styles/menu.css?v='.$theme_version);
 
 switch ($PAGE_NAME[0]) {
     // public
     case 'actualite':
-        $STYLES[] = new Isou\Helpers\Style(URL.'/themes/classic/styles/news.css');
+        $STYLES[] = new Style(URL.'/themes/classic/styles/news.css?v='.$theme_version);
         break;
     case 'liste':
-        $STYLES[] = new Isou\Helpers\Style(URL.'/themes/classic/styles/table.css');
-        $STYLES[] = new Isou\Helpers\Style(URL.'/themes/classic/styles/contact.css');
+        $STYLES[] = new Style(URL.'/themes/classic/styles/table.css?v='.$theme_version);
+        $STYLES[] = new Style(URL.'/themes/classic/styles/contact.css?v='.$theme_version);
         break;
     case 'tableau':
-        $STYLES[] = new Isou\Helpers\Style(URL.'/themes/classic/styles/board.css');
+        $STYLES[] = new Style(URL.'/themes/classic/styles/board.css?v='.$theme_version);
         break;
     case 'journal':
-        $STYLES[] = new Isou\Helpers\Style(URL.'/themes/classic/styles/record.css');
+        $STYLES[] = new Style(URL.'/themes/classic/styles/record.css?v='.$theme_version);
         break;
     case 'calendrier':
-        $STYLES[] = new Isou\Helpers\Style(URL.'/themes/classic/styles/calendar.css');
-        $STYLES[] = new Isou\Helpers\Style(URL.'/themes/classic/styles/news.css');
+        $STYLES[] = new Style(URL.'/themes/classic/styles/calendar.css?v='.$theme_version);
+        $STYLES[] = new Style(URL.'/themes/classic/styles/news.css?v='.$theme_version);
         break;
     case 'contact':
-        $STYLES[] = new Isou\Helpers\Style(URL.'/themes/classic/styles/contact.css');
+        $STYLES[] = new Style(URL.'/themes/classic/styles/contact.css?v='.$theme_version);
         break;
     case 'rss':
-        $STYLES[] = new Isou\Helpers\Style(URL.'/themes/classic/styles/rss_config.css');
+        $STYLES[] = new Style(URL.'/themes/classic/styles/rss_config.css?v='.$theme_version);
         break;
     // private
     case 'evenements':
-        $STYLES[] = new Isou\Helpers\Style(URL.'/themes/classic/styles/events.css');
+        $STYLES[] = new Style(URL.'/themes/classic/styles/events.css?v='.$theme_version);
         break;
     case 'annonce':
-        $STYLES[] = new Isou\Helpers\Style(URL.'/themes/classic/styles/annonce.css');
+        $STYLES[] = new Style(URL.'/themes/classic/styles/annonce.css?v='.$theme_version);
         break;
     case 'statistiques':
-        $STYLES[] = new Isou\Helpers\Style(URL.'/themes/classic/styles/stats.css');
-        $STYLES[] = new Isou\Helpers\Style(URL.'/themes/classic/styles/common.css', 'print');
-        $STYLES[] = new Isou\Helpers\Style(URL.'/themes/classic/styles/stats_print.css', 'print');
+        $STYLES[] = new Style(URL.'/themes/classic/styles/stats.css?v='.$theme_version);
+        $STYLES[] = new Style(URL.'/themes/classic/styles/common.css?v='.$theme_version, 'print');
+        $STYLES[] = new Style(URL.'/themes/classic/styles/stats_print.css?v='.$theme_version, 'print');
         break;
     case 'services':
-        $STYLES[] = new Isou\Helpers\Style(URL.'/themes/classic/styles/services.css');
+        $STYLES[] = new Style(URL.'/themes/classic/styles/services.css?v='.$theme_version);
         break;
     case 'dependances':
-        $STYLES[] = new Isou\Helpers\Style(URL.'/themes/classic/styles/dependencies.css');
+        $STYLES[] = new Style(URL.'/themes/classic/styles/dependencies.css?v='.$theme_version);
         break;
     case 'categories':
-        $STYLES[] = new Isou\Helpers\Style(URL.'/themes/classic/styles/categories.css');
+        $STYLES[] = new Style(URL.'/themes/classic/styles/categories.css?v='.$theme_version);
         break;
     case 'configuration':
-        $STYLES[] = new Isou\Helpers\Style(URL.'/themes/classic/styles/configuration.css');
+        $STYLES[] = new Style(URL.'/themes/classic/styles/configuration.css?v='.$theme_version);
         break;
     case 'aide':
-        $STYLES[] = new Isou\Helpers\Style(URL.'/themes/classic/styles/help.css');
+        $STYLES[] = new Style(URL.'/themes/classic/styles/help.css?v='.$theme_version);
         break;
 }
