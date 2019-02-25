2d12a89a36c530bc0027f66c48ee7188be07545c
Julien Boulen <julien.boulen@univ-rennes2.fr>
2019-02-25 16:24:35 +0100

    refactor(bootstrap): fusionne toutes les feuilles dans style dans le fichier common.css

--- www/themes/bootstrap3/theme.php
+++ www/themes/bootstrap3/theme.php
@@ -6,19 +6,3 @@ $theme_version = '1';
 
 $STYLES[] = new Style('//unpkg.com/bootstrap@3.3/dist/css/bootstrap.min.css');
 $STYLES[] = new Style(URL.'/themes/bootstrap3/css/common.css?v='.$theme_version);
-
-if (isset($current_page->url) === true) {
-    switch ($current_page->url) {
-        case 'actualite':
-            $STYLES[] = new Style(URL.'/themes/bootstrap3/css/news.css?v='.$theme_version);
-            break;
-        case 'calendrier':
-            $STYLES[] = new Style(URL.'/themes/bootstrap3/css/calendar.css?v='.$theme_version);
-            break;
-        case 'dependances':
-            $STYLES[] = new Style(URL.'/themes/bootstrap3/css/dependencies.css?v='.$theme_version);
-            break;
-        case 'rss':
-            $STYLES[] = new Style(URL.'/themes/bootstrap3/css/rss.css?v='.$theme_version);
-    }
-}
--- www/themes/rennes2/theme.php
+++ www/themes/rennes2/theme.php
@@ -10,22 +10,6 @@ $STYLES[] = new Style('//static.univ-rennes2.fr/bootstrap/3.3/rennes2/bootstrap.
 $STYLES[] = new Style(URL.'/themes/bootstrap3/css/common.css?v='.$theme_version);
 $STYLES[] = new Style(URL.'/themes/rennes2/css/common.css?v='.$theme_version);
 
-if (isset($current_page->url) === true) {
-    switch ($current_page->url) {
-        case 'actualite':
-            $STYLES[] = new Style(URL.'/themes/bootstrap3/css/news.css?v='.$theme_version);
-            break;
-        case 'calendrier':
-            $STYLES[] = new Style(URL.'/themes/bootstrap3/css/calendar.css?v='.$theme_version);
-            break;
-        case 'dependances':
-            $STYLES[] = new Style(URL.'/themes/bootstrap3/css/dependencies.css?v='.$theme_version);
-            break;
-        case 'rss':
-            $STYLES[] = new Style(URL.'/themes/bootstrap3/css/rss.css?v='.$theme_version);
-    }
-}
-
 $SCRIPTS[] = new Script('//static.univ-rennes2.fr/barre-ent/script.min.js');
 
 $ADDITIONAL_CONTENT = <<<EOF
