b8a93305d04d5baa087a597dc74e74d297078126
Julien Boulen <julien.boulen@univ-rennes2.fr>
2020-11-25 16:46:52 +0100

    feat(thruk): ajoute une information lorsqu'un service Thruk enregistré dans Isou a été retiré de la source

--- plugins/monitoring/thruk/php/list.php
+++ plugins/monitoring/thruk/php/list.php
@@ -2,6 +2,21 @@
 
 use UniversiteRennes2\Isou\Service;
 
+// Charge la liste des services Thruk distants.
+$sources = array();
+$cache_path = PRIVATE_PATH.'/cache/plugins/monitoring/thruk';
+if (is_readable($cache_path.'/services.json') === true) {
+    $cache = file_get_contents($cache_path.'/services.json');
+    $cache = json_decode($cache, $array = true);
+
+    if (empty($cache) === false) {
+        foreach ($cache as $data) {
+            $sources[$data['name']] = $data['name'];
+        }
+    }
+}
+
+// Charge les services Thruk enregistrés dans la base de données Isou.
 $services = Service::get_records(array('plugin' => $plugin->id));
 
 foreach ($services as $service) {
@@ -10,6 +25,10 @@ foreach ($services as $service) {
     $service->set_reverse_dependencies();
     if (isset($service->reverse_dependencies[0]) === false) {
         $service->notes[] = 'Service non rattaché à un service Isou';
+    }
+
+    if (isset($sources[$service->name]) === false) {
+        $service->notes[] = 'Service retiré du serveur Thruk';
     }
 }
 
