1bddd87274fc07d7489a9edc9d0be3b13ec96935
Julien Boulen <julien.boulen@univ-rennes2.fr>
2021-06-11 15:11:25 +0200

    fix(settings): enregistre correctement la variable 'site_url'

--- php/settings/general.php
+++ php/settings/general.php
@@ -21,11 +21,6 @@ if ($handle !== false) {
 }
 ksort($themes);
 
-// Hack pour pré-initialiser la variable site_url.
-if (empty($CFG['site_url']) === true) {
-    $CFG['site_url'] = URL.'/';
-}
-
 foreach (array('site_name', 'site_header', 'site_url', 'tolerance', 'theme') as $key) {
     if (isset($_POST[$key]) === true) {
         $value = htmlentities($_POST[$key], ENT_QUOTES, 'UTF-8');
@@ -35,6 +30,11 @@ foreach (array('site_name', 'site_header', 'site_url', 'tolerance', 'theme') as 
             }
         }
     }
+}
+
+// Hack pour pré-initialiser la variable site_url.
+if (empty($CFG['site_url']) === true) {
+    $CFG['site_url'] = URL.'/';
 }
 
 if (isset($_POST['menu_default'], $MENUS->public[$_POST['menu_default']]) === true) {
