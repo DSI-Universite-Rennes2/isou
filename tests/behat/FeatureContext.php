da7f938db98d5e789e751d8afc5acf47d3e0435c
Julien Boulen <julien.boulen@univ-rennes2.fr>
2025-06-25 17:26:12 +0200

    test(behat): initialise une suite de tests behat

---
+++ tests/behat/FeatureContext.php
@@ -0,0 +1,44 @@
+<?php // phpcs:disable Generic.Files.LowercasedFilename.NotFound
+
+// phpcs:disable PSR1.Classes.ClassDeclaration.MissingNamespace
+
+declare(strict_types=1);
+
+use Behat\Behat\Tester\Exception\PendingException;
+use Behat\Gherkin\Node\PyStringNode;
+use Behat\Gherkin\Node\TableNode;
+use Behat\MinkExtension\Context\MinkContext;
+use Behat\Step\Given;
+use Behat\Step\Then;
+use Behat\Step\When;
+
+/**
+ * Définit des verbes spécifiques pour naviguer sur le site testé.
+ *
+ * Scripts qui peuvent aider :
+ *   - vendor/friends-of-behat/mink-extension/src/Behat/MinkExtension/Context/MinkContext.php
+ *   - vendor/friends-of-behat/mink-extension/i18n/fr.xliff
+ */
+class FeatureContext extends MinkContext {
+    /**
+     * Initializes context.
+     *
+     * Every scenario gets its own context instance.
+     * You can also pass arbitrary arguments to the
+     * context constructor through behat.yml.
+     */
+    public function __construct() {
+    }
+
+    /**
+     * Alias pour texte "Je suis <lien>".
+     *
+     * @param string $link Lien recherché. La valeur peut représenter le libellé du lien, l'attribut id, title ou alt.
+     *
+     * @When   /^(?:|je )clique sur le lien "(?P<link>(?:[^"]|\\")*)"$/
+     * @return void
+     */
+    public function jeCliqueSurLeLien(string $link): void {
+        $this->clickLink($link);
+    }
+}
---
+++ tests/behat/crawl.feature
@@ -0,0 +1,7 @@
+# language: fr
+# @navigation
+Fonctionnalité: Navigue sur toutes les pages publiques.
+
+  Scénario: Lorsque je navigue sur la page d'accueil, je dois voir le message "Liste des interruptions en cours.".
+    Sachant que je suis sur la page d'accueil
+    Alors je devrais voir "Liste des interruptions en cours."
